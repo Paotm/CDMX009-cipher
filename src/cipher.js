f34261480930745e351efff7143b7b4dcc98fa5c
Paotm <60703539+Paotm@users.noreply.github.com>
2020-02-12 14:11:06 -0600

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,17 @@
-const cipher = {encode, decode
+
+ const cipher = {encode, decode
 };
+export default cipher;
 // se declara la funcion para codificar
 function encode (offset, msj) {
   let outcome=""; //variable para dar el mensaje codificado
   let entradascii;
   let salidascii;
-  for (let a= 0; a < msj.length; a++){ // da las vueltas para que haga el desplazamiento en cada caracter
+  for (var a= 0; a < msj.length; a++){ // da las vueltas para que haga el desplazamiento en cada caracter
     entradascii = msj.chartCodeAt(a); // obtiene el codigo ascii de los caracteres ingresados
-  salidascii =((entradascii - 65 + parseInt(offset))% 26 + 65 ); // formula que nos dan como pista para obtener la posicion del desplazamiento
-  outcome += string.fromCharCode (salidascii); }//regresa los caracteres con la posiscion de desplazamiento
+    salidascii=(entradascii - 65 + parseInt(offset))% 26 + 65 ; // formula que nos dan como pista para obtener la posicion del desplazamiento
+  outcome += String.fromCharCode (salidascii);
+}//regresa los caracteres con la posiscion de desplazamiento
   return(outcome);
 
 }
@@ -16,14 +19,12 @@ function encode (offset, msj) {
 function decode (offset, msj) {
   let outcome="";
   let entradascii;
-  let salidascii;
-  for (let a= 0; a < msj.length; a++){
+ let salidascii;
+  for (var a= 0; a < msj.length; a++){
     entradascii = msj.chartCodeAt(a);
-  salidascii =((entradascii + 65 - parseInt(offset))% 26 + 65 );
-  outcome += string.fromCharCode (salidascii);}
+  salidascii =(entradascii + 65 - parseInt(offset))% 26 + 65 ;
+  outcome += String.fromCharCode (salidascii);
+}
   return(outcome);
 
 }
-
-
-export default cipher;
