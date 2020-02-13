f1612c10a8e538e5a2e397467aa9011f2c897ed4
paotm <ipao.tm2202@gmail.com>
2020-02-13 12:36:20 -0600

    ultimos cambios, pasa pruebas y estilo casi listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
 const cipher = {
-  // ...
-};
-
+   encode,
+   decode
+}
 export default cipher;
+// se declara la funcion para codificar
+function encode (offset, palabra) {
+let outcome=""; //variable para dar el mensaje codificado
+
+  for (let a= 0; a < palabra.length; a++){ // da las vueltas para que haga el desplazamiento en cada caracter
+   let entradascii = palabra.charCodeAt(a); // obtiene el codigo ascii de los caracteres ingresados
+  entradascii=(entradascii- 65+parseInt(offset))%26+65; // formula que nos dan como pista para obtener la posicion del desplazamiento
+  outcome += String.fromCharCode (entradascii);
+}//regresa los caracteres con la posiscion de desplazamiento
+ return(outcome);
+}
+
+// Se declara la funcion para decodificar
+function decode (offset, palabra) {
+  let outcome="";
+    for (let a= 0; a < palabra.length; a++){
+let  entradascii = palabra.charCodeAt(a);
+   entradascii =(entradascii+65-parseInt(offset))%26+65;
+  outcome += String.fromCharCode (entradascii);
+}
+  return(outcome);
+}
