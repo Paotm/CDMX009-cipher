8b09b238a2fd66e626fb0d80c6c7ad7a722c60e5
Paotm <60703539+Paotm@users.noreply.github.com>
2020-02-13 14:01:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let palabra= document.getElementById("palabra");
+let offset = document.getElementById("offset");
+let pantalla1 =document.getElementById("bienvenida");
+let pantalla2 =document.getElementById("datos");
+let pantalla3 =document.getElementById("final");
