eff835a2ec296aa50656d9ca96f19c77529d9824
Paotm <60703539+Paotm@users.noreply.github.com>
2020-02-14 10:15:54 -0600

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,12 @@
-
- const cipher = {
+const cipher = {
    encode,
    decode
 }
-export default cipher;// se declara la funcion para codificar
+export default cipher;
+// se declara la funcion para codificar
 function encode (offset, palabra) {
 let outcome=""; //variable para dar el mensaje codificado
-// let salidascii;
+
   for (let a= 0; a < palabra.length; a++){ // da las vueltas para que haga el desplazamiento en cada caracter
    let entradascii = palabra.charCodeAt(a); // obtiene el codigo ascii de los caracteres ingresados
   entradascii=(entradascii- 65+parseInt(offset))%26+65; // formula que nos dan como pista para obtener la posicion del desplazamiento
@@ -18,9 +18,7 @@ let outcome=""; //variable para dar el mensaje codificado
 // Se declara la funcion para decodificar
 function decode (offset, palabra) {
   let outcome="";
-  //let entradascii;
- //let salidascii;
-  for (let a= 0; a < palabra.length; a++){
+    for (let a= 0; a < palabra.length; a++){
 let  entradascii = palabra.charCodeAt(a);
    entradascii =(entradascii+65-parseInt(offset))%26+65;
   outcome += String.fromCharCode (entradascii);
