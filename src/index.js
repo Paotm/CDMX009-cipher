c36af25ca1be49451c57efa444b835788d5db012
Paolatm <ipao.tm2202@gmail.com>
2020-09-18 15:40:41 -0500

    CSS

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 console.log(cipher);
 
-let word= document.getElementById("palabra");
+let word = document.getElementById("palabra");
 let offset = document.getElementById("offset");
-let pantalla1 =document.getElementById("bienvenida");
-let pantalla2 =document.getElementById("datos");
-let pantalla3 =document.getElementById("final");
+let pantalla1 = document.getElementById("screenOne");
+let pantalla2 = document.getElementById("datos");
+let pantalla3 = document.getElementById("final");
 
-function iniciar(){
-pantalla1.style.display="none";
-pantalla2.style.display="block";
+function iniciar() {
+  pantalla1.style.display = "none";
+  pantalla2.style.display = "block";
 }
 
-function cifrar()
-{
-let texto = cipher.encode(offset.value, word.value);
- document.getElementById("txtfinal").innerText=texto;
-pantalla3.style="display:block";
+function cifrar() {
+  let texto = cipher.encode(offset.value, word.value);
+  document.getElementById("txtfinal").innerText = texto;
+  pantalla2.style.display = "none";
+  pantalla3.style = "display:block";
 }
 
-function descifrar()
-{
-let texto = cipher.decode(offset.value, word.value);
-document.getElementById("txtfinal").innerText=texto;
-pantalla3.style="display:block";
+function descifrar() {
+  let texto = cipher.decode(offset.value, word.value);
+  document.getElementById("txtfinal").innerText = texto;
+  pantalla2.style.display = "none";
+  pantalla3.style = "display:block";
 }
 
-function regreso(){
+function regreso() {
   document.location.reload(true);
 }
 
 document.getElementById("comienzo").addEventListener("click", iniciar);
 document.getElementById("codi").addEventListener("click", cifrar);
-document.getElementById("decodi").addEventListener("click",descifrar);
-document.getElementById("regreso").addEventListener("click",regreso);
+document.getElementById("decodi").addEventListener("click", descifrar);
+document.getElementById("regreso").addEventListener("click", regreso);
